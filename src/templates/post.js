a8b8fb65960c70366442b9bbca98f0cdab376e83
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2020-02-24 15:52:03 +0100

    Add better SEO

--- src/templates/post.js
+++ src/templates/post.js
@@ -4,10 +4,34 @@ import SEO from "../components/seo"
 import Layout from "../components/layout"
 export default function Template({ data }) {
   const { frontmatter, html, excerpt } = data.markdownRemark
+  const { deployURL, author } = data.site.siteMetadata
   return (
     <Layout>
       <div className="blog-post">
-        <SEO description={excerpt} title={frontmatter.title} />
+        <SEO description={excerpt} title={frontmatter.title}>
+          <script type="application/ld+json">
+            {JSON.stringify(
+              {
+                "@context": "http://schema.org",
+                "@type": "Article",
+                "headline": frontmatter.title,
+                "url": deployURL + frontmatter.path,
+                "datePublished": frontmatter.date,
+                "author": {
+                  "@context": "http://schema.org",
+                  "@type": "Person",
+                  ...author,
+                },
+                "publisher": {
+                  "@context": "http://schema.org",
+                  "@type": "Person",
+                  ...author,
+                },
+                "image": deployURL + data.file.publicURL
+              }
+            )}
+          </script>
+        </SEO>
         <h1>{frontmatter.title}</h1>
         <h2>{frontmatter.date}</h2>
         <div
@@ -20,14 +44,27 @@ export default function Template({ data }) {
 }
 export const pageQuery = graphql`
   query($path: String!) {
+    file(name: { eq: "face" }) {
+      publicURL
+    }
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
       frontmatter {
-        date(formatString: "MMMM DD, YYYY")
+        date
         path
         title
       }
       excerpt
+    }
+    site {
+      siteMetadata {
+        author {
+          familyName
+          givenName
+          name
+        }
+        deployURL
+      }
     }
   }
 `
